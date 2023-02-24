60316939ad9477106bec6c055b0cc0fb71857fc4
Hsnakdag <75378924+Hsnakdag@users.noreply.github.com>
2023-02-24 15:41:27 +0300

    crud operation

--- LstSurveyApi/Models/QuestionUnitSurvey.cs
+++ LstSurveyApi/Models/QuestionUnitSurvey.cs
@@ -1,21 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace LstSurveyApi.Models;
 
 public partial class QuestionUnitSurvey
 {
+    [JsonPropertyName("question_id")]
     [Key]
     public int QuestionId { get; set; }
 
+    [JsonPropertyName("unit_id")]
     public int UnitId { get; set; }
 
     public int SurveyId { get; set; }
 
+    [ForeignKey("QuestionId")]
     public virtual Question Question { get; set; } = null!;
 
     public virtual Survey Survey { get; set; } = null!;
 
+    [ForeignKey("UnitId")]
     public virtual Unit Unit { get; set; } = null!;
 }
