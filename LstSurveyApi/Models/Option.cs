b23c613ca8404767d90a09ad2bc742968db57aaa
Hsnakdag <75378924+Hsnakdag@users.noreply.github.com>
2023-03-03 10:56:29 +0300

    Option Crud

---
+++ LstSurveyApi/Models/Option.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LstSurveyApi.Models;
+
+public partial class Option
+{
+    public int OptionId { get; set; }
+
+    public string? OptionText { get; set; }
+
+    public int? QuestionId { get; set; }
+
+    public virtual Question? Question { get; set; }
+}
